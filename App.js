3cb8ffae759e0dfc169e94ea31d83453a805804e
Haaris Mian <mail@haarismian.com>
2019-08-18 18:41:26 -0400

    Initial Commit

---
+++ App.js
@@ -0,0 +1,67 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+import {StyleSheet} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+// UI Kitten
+import {mapping, light as lightTheme} from '@eva-design/eva';
+import {ApplicationProvider, Layout} from 'react-native-ui-kitten';
+
+const App = () => {
+  return (
+    <ApplicationProvider mapping={mapping} theme={lightTheme}>
+      <Layout style={{flex: 1}} />
+      <Text>test</Text>
+    </ApplicationProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
